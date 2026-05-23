96736c4fefb909c8e653ec07e26ae31682ec06d5
Fadillaa-syifa <asyifafadilla56@gmail.com>
2026-05-23 11:26:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ elif page == "Settings":
             st.session_state.username = None
             st.session_state.data = None
             st.success("You have been logged out.")
-            st.rerun()
+            st.experimental_rerun()
 
     with st.expander("📝 Update Preferences"):
         theme = st.selectbox("Pilih tema dashboard", ["Default", "Dark", "Colorful"])
